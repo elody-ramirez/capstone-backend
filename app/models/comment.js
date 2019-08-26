3c932ea7fc27d95886d11375db7621767f289f47
elody-ramirez <elody.ramirez@gmail.com>
2019-08-26 12:20:02 -0400

    Add Comment model
    The comment model includes a text property, which will be in string
    format. It will also point to the user who created the comment and the
    post that the comment refers to.

---
+++ app/models/comment.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+
+const commentSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  text: {
+    type: String,
+    required: true
+  },
+  post: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Post'
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
+}, {
+  timestamps: true
+})
+
+module.exports = mongoose.model('Comment', commentSchema)
